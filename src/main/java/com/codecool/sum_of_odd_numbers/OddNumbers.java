d08f00a43d7638767c37d4ff64a0afb49ed3c386
Straub Levente <slevy57@gmail.com>
2021-03-24 15:10:57 +0100

    algoo ready

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,14 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+
+        int sum = 0;
+
+        for (Integer number : numbers) {
+            if (number % 2 == 1 || number % 2 == -1) {
+                sum = sum + number;
+            }
+        }
+        return sum;
     }
 }
